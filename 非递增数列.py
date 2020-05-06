bb7a40b13fe3b55c8ab994fbb3971f33af547ef2
Jack xie <37534408+xieyixiao@users.noreply.github.com>
2020-05-05 23:26:11 -0500

    Update 非递增数列.py
    fix issue

--- 非递增数列.py
+++ 非递增数列.py
@@ -1,10 +1,10 @@
-sequence = [1, 3, 4, 5, 4, 3, 8, 9, 9, ]
-s = len(sequence) - 1
+sequence = [1, 3, 4, 5, 4, 3, 8, 9, 9]
+s = len(sequence)
 k = 0
 if sequence[0] > sequence[1]:
     sequence[0] = sequence[1]
     k += 1
-for i in range(1, s + 1):
+for i in range(1, s):
     if sequence[i] < sequence[i - 1]:
         sequence[i] = sequence[i - 1]
         k += 1
